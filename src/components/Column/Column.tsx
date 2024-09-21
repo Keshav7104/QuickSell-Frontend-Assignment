8bb937096226ece4d4d7fd747128efd7e5b70d18
Keshav <keshavbhutaani1360@gmail.com>
2024-09-21 12:14:07 +0530

    added animation

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,54 +1,79 @@
-import React, { useMemo } from 'react';
-import Card from '../Card';
-import "./column.css"
-import { GrAdd } from 'react-icons/gr';
-import { LuMoreHorizontal } from 'react-icons/lu';
-import { Ticket, User } from '../../interfaces';
-import { getPriorityIcon, getStatusIcon } from '../../utils/helper';
-import UserIcon from '../UserIcon';
+import React, { useMemo, useEffect, useState } from "react";
+import Card from "../Card";
+import "./column.css";
+import { GrAdd } from "react-icons/gr";
+import { LuMoreHorizontal } from "react-icons/lu";
+import { Ticket, User } from "../../interfaces";
+import { getPriorityIcon, getStatusIcon } from "../../utils/helper";
+import UserIcon from "../UserIcon";
 
+function Column({
+  tickets,
+  grouping,
+  groupBy,
+  userIdToData,
+}: {
+  tickets: Ticket[];
+  grouping: string;
+  groupBy: string;
+  userIdToData: Record<string, User>;
+}) {
+  const [animate, setanimate] = useState(false);
+  const title = useMemo(() => {
+    if (grouping === "status") return groupBy;
+    if (grouping === "priority") return groupBy;
+    if (grouping === "user") return userIdToData[groupBy].name;
+  }, [grouping, groupBy]);
 
-function Column({ tickets, grouping, groupBy, userIdToData }: { tickets: Ticket[], grouping: string, groupBy: string, userIdToData: Record<string, User> }) {
+  const icon = useMemo(() => {
+    if (grouping === "status") return getStatusIcon(groupBy);
+    if (grouping === "priority") return getPriorityIcon(groupBy);
+    if (grouping === "user")
+      return (
+        <UserIcon
+          name={userIdToData[groupBy].name}
+          available={userIdToData[groupBy].available}
+        />
+      );
+  }, [grouping, groupBy]);
 
-    const title = useMemo(() => {
-        if (grouping === "status")
-            return groupBy;
-        if (grouping === "priority")
-            return groupBy;
-        if (grouping === "user")
-            return userIdToData[groupBy].name;
-    }, [grouping, groupBy]);
+  useEffect(() => {
+    setanimate(false);
+    const timer = setTimeout(() => {
+      setanimate(true);
+    }, 50);
+    return () => clearTimeout(timer);
+  }, [tickets]);
 
-    const icon = useMemo(() => {
-        if (grouping === "status")
-            return getStatusIcon(groupBy);
-        if (grouping === "priority")
-            return getPriorityIcon(groupBy);
-        if (grouping === "user")
-            return <UserIcon name={userIdToData[groupBy].name} available={userIdToData[groupBy].available} />
-    }, [grouping, groupBy])
-
-
-    return (
-        <div className='column'>
-            <div className='column-header'>
-                <div className='column-header-left-container'>
-                    {icon}
-                    <div className='column-title'>
-                        {title}
-                        <span className='count'>{tickets.length}</span>
-                    </div>
-                </div>
-                <div className='column-header-right-container'>
-                    <GrAdd color="#797d84" size={12} />
-                    <LuMoreHorizontal color="#797d84" size={14} />
-                </div>
-            </div>
-            <div className='cards-container'>
-                {tickets.map((ticket: Ticket) => <Card key={ticket.id} ticket={ticket} userData={userIdToData[ticket.userId]} hideStatusIcon={grouping === "status"} hideProfileIcon={grouping === "user"} />)}
-            </div>
+  return (
+    <div className="column">
+      <div className="column-header">
+        <div className="column-header-left-container">
+          {icon}
+          <div className="column-title">
+            {title}
+            <span className="count">{tickets.length}</span>
+          </div>
         </div>
-    );
+        <div className="column-header-right-container">
+          <GrAdd color="#797d84" size={12} />
+          <LuMoreHorizontal color="#797d84" size={14} />
+        </div>
+      </div>
+      <div className="cards-container">
+        {tickets.map((ticket: Ticket,index:number) => (
+          <div key={ticket.id} style={{animationDelay : `${index*0.2}s`}} className={` tickets ${animate ? 'ani':''}`}>
+            <Card
+              ticket={ticket}
+              userData={userIdToData[ticket.userId]}
+              hideStatusIcon={grouping === "status"}
+              hideProfileIcon={grouping === "user"}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Column;
--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,14 +1,25 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useMemo, useEffect,useState } from 'react';
 import './grid.css'
 import Column from '../Column/Column';
 import { Ticket, User } from '../../interfaces';
 
 function Grid({ gridData, grouping, userIdToData }: { gridData: Record<string, Ticket[]>, grouping: string, userIdToData: Record<string, User> }) {
     const keys: string[] = useMemo(() => Object.keys(gridData), [gridData]);
-
+    const [animate, setanimate] = useState(false);
+    useEffect(()=>{
+        setanimate(false);
+        const timer = setTimeout(() => {
+            setanimate(true);
+        }, 50);
+        return ()=> clearTimeout(timer);
+    },[gridData]);
     return (
         <div className='grid'>
-            {keys.map((k: string) => <Column key={k} tickets={gridData[k] as Ticket[]} grouping={grouping} groupBy={k} userIdToData={userIdToData} />)}
+            {keys.map((k: string,index:number) => (
+                <div key={k} style={{animationDelay : `${index * 0.2}s`}} className={`cols ${animate ? 'animate':''}`}>
+                    <Column tickets={gridData[k] as Ticket[]} grouping={grouping} groupBy={k} userIdToData={userIdToData} />
+                </div>
+            ))}
         </div>
     );
 }
